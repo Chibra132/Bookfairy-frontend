c565452f61820673f1cf2fbb9ea567894d13d702
Andrew J <andrewsjorquera@gmail.com>
2019-04-24 23:25:00 +0000

    dynamic jumbotron

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -4,6 +4,8 @@ import { Jumbotron, Container } from "reactstrap";
 import HomeCarousel from "../component/Carousel.jsx";
 import BlogCard from "../component/BlogCards.jsx";
 import { Context } from "../store/appContext.jsx";
+import { Card, Button, CardTitle, CardText, Row, Col } from "reactstrap";
+import MainBlog from "../views/Mainblog.jsx";
 
 const Home = props => {
 	// create 2 aux variables mapping into jumbotron and cards
@@ -12,32 +14,35 @@ const Home = props => {
 		<Context.Consumer>
 			{({ store, actions }) => {
 				const jumboData = store.blogcards[0];
+				console.log(jumboData);
 				const blogCardsData = store.blogcards.slice(1, 4);
 				return (
 					<div>
-						<Jumbotron fluid id="jumboHome">
-							<Container fluid>
-								<h1 className="display-3">
-									A song of Ice and Fire
-								</h1>
-								<p className="lead">
-									Reading the books that inspired the
-									sensational show
-								</p>
-							</Container>
-						</Jumbotron>
-						{/*console.log(store);*/}
-						{blogCardsData.map(blog => {
-							return (
-								<BlogCard
-									key={blog.ID}
-									image={blog.acf.image}
-									title={blog.post_title}
-									content={blog.post_content}
-									footer={blog.post_date}
-								/>
-							);
-						})}
+						{typeof jumboData !== "undefined" ? (
+							<MainBlog
+								image={jumboData.acf.image}
+								title={jumboData.post_title}
+								content={jumboData.post_content}
+							/>
+						) : (
+							<MainBlog image="" title="" content="" />
+						)}{" "}
+						<Row
+							className="justify-content-center mb-2"
+							id="blogCardRow"
+							key="1">
+							{blogCardsData.map(blog => {
+								return (
+									<BlogCard
+										key={blog.ID}
+										image={blog.acf.image || ""}
+										title={blog.post_title}
+										content={blog.post_content}
+										footer={blog.post_date}
+									/>
+								);
+							})}
+						</Row>
 						<HomeCarousel />
 					</div>
 				);
